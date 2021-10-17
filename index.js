0ec07335f32e4a92c2516df974d89dad2496f306
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-10-17 17:43:42 -0500

    Added init function and prompt for employee type. This prompt throws to one of three dummy functions to generate further prompts for each subclass.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const Inquirer = require('inquirer');
+const inquirer = require('inquirer');
+const { type } = require('os');
+
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What type of employee would you like to add?',
+                choices: ['Engineer', 'Manager', 'Intern'],
+                name: 'type'
+            }
+        ])
+        .then(response => {
+            // console.log(response.type)
+            switch (response.type) {
+                case 'Engineer':
+                    console.log('Engineer');
+                    generateEngineer();
+                    break;
+                case 'Manager':
+                    console.log('Manager');
+                    generateManager();
+                    break;
+                case 'Intern':
+                    console.log('Intern');
+                    generateIntern();
+                    break;
+            
+                default:''
+                    break;
+            }
+
+        })
+}
+
+init();
